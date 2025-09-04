254013cd4df32146eead4e942ec9fe3c43bee041
tomamac <mek777@hotmail.co.th>
2025-09-05 04:34:04 +0700

    Fix : refactor hub exceptions

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,72 +1,92 @@
-﻿using Chariot.Models;
+﻿using Chariot.Entities;
 using Chariot.Services;
 using Microsoft.AspNetCore.SignalR;
-using System;
 using System.Security.Claims;
 
 namespace Chariot.Hubs
 {
     public class ChatHub(IChatService chatService) : Hub
     {
+        public override async Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+
+            var userId = GetUserId() ?? throw new HubException("Invalid user ID claim");
+            await chatService.SetOnlineStatusAsync(userId, true);
+
+            var chatroomList = await chatService.GetChatroomsAsync(userId);
+            foreach (var chatroom in chatroomList)
+            {
+                await Groups.AddToGroupAsync(Context.ConnectionId, chatroom.Id.ToString()!);
+            }
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            var userId = GetUserId();
+            if (userId is not null)
+            {
+                await chatService.SetOnlineStatusAsync((int)userId, false);
+            }
+
+            await base.OnDisconnectedAsync(exception);
+        }
+
         public async Task CreateRoom(string roomName)
         {
-            if (int.TryParse(Context.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value, out int userId))
-            {
-                var chatroom = await chatService.CreateChatAsync(userId, roomName);
-                if (chatroom is null)
-                {
-                    throw new HubException("Failed to create chat");
-                }
-                await Groups.AddToGroupAsync(Context.ConnectionId, chatroom.Chatroom.Id.ToString()!);
-                await Clients.Caller.SendAsync("RoomCreated", chatroom);
-            }
-            else await Clients.Caller.SendAsync("Error", new { message = "Invalid user ID claim" });
+            var userId = GetUserId() ?? throw new HubException("Invalid user ID claim");
+            var chatroom = await chatService.CreateChatAsync(userId, roomName) ?? throw new HubException("Failed to create chat");
+
+            await Groups.AddToGroupAsync(Context.ConnectionId, chatroom.Id.ToString()!);
+            await Clients.Caller.SendAsync("RoomCreated", chatroom);
         }
 
         public async Task JoinRoom(string roomCode)
         {
-            if (int.TryParse(Context.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value, out int userId))
+            var userId = GetUserId() ?? throw new HubException("Invalid user ID claim");
+            var (result, chatroom) = await chatService.JoinChatAsync(userId, roomCode);
+
+            switch (result)
             {
-                var roomId = await chatService.JoinChatAsync(userId, roomCode);
-                if (roomId is null)
-                {
-                    await Clients.Caller.SendAsync("Error", new { message = "Room not found" });
-                    return;
-                }
-                await Groups.AddToGroupAsync(Context.ConnectionId, roomId.ToString()!);
+                case (JoinChatResult.NotFound):
+                    throw new HubException("Room not found");
+                case (JoinChatResult.AlreadyJoined):
+                    throw new HubException("Already joined");
+                default:
+                    await Groups.AddToGroupAsync(Context.ConnectionId, chatroom!.Id.ToString());
+                    await Clients.Caller.SendAsync("RoomJoined", chatroom);
+
+                    var sysMessage = await chatService.SaveSystemMessageAsync(chatroom.Id,
+                        $"{Context.User?.FindFirst(ClaimTypes.Name)?.Value!} has joined");
+                    if (sysMessage is not null)
+                        await Clients.Group(chatroom.Id.ToString()).SendAsync("ReceiveMessage", sysMessage);
+
+                    break;
             }
-            else await Clients.Caller.SendAsync("Error", new { message = "Invalid user ID claim" });
         }
 
         public async Task SendMessage(int roomId, string message)
         {
-            if (int.TryParse(Context.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value, out int userId))
-            {
-                var messageRes = await chatService.SaveMessageAsync(userId, roomId, message);
-                if (messageRes is null)
-                {
-                    await Clients.Caller.SendAsync("Error", new { message = "Failed to send message" });
-                    return;
-                }
-                await Clients.Group(roomId.ToString()).SendAsync("ReceiveMessage", messageRes);
-            }
-            else await Clients.Caller.SendAsync("Error", new { message = "Invalid user ID claim" });
+            var userId = GetUserId() ?? throw new HubException("Invalid user ID claim");
+            var messageRes = await chatService.SaveMessageAsync(userId, roomId, message) ?? throw new HubException("Failed to send message");
+            await Clients.Group(roomId.ToString()).SendAsync("ReceiveMessage", messageRes);
         }
 
         public async Task LeaveRoom(int roomId)
         {
-            if (int.TryParse(Context.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value, out int userId))
-            {
-                var leaver = await chatService.LeaveChatAsync(userId, roomId);
-                if (leaver is null)
-                {
-                    await Clients.Caller.SendAsync("Error", new { message = "Failed to leave chat" });
-                    return;
-                }
-                await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomId.ToString());
-                await Clients.Group(roomId.ToString()).SendAsync("ChatEvent", new { message = $"{leaver} has left the chat"});
-            }
-            else await Clients.Caller.SendAsync("Error", new { message = "Invalid user ID claim" });
+            var userId = GetUserId() ?? throw new HubException("Invalid user ID claim");
+            var leaver = await chatService.LeaveChatAsync(userId, roomId) ?? throw new HubException("Failed to leave chat");
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomId.ToString());
+
+            var sysMessage = await chatService.SaveSystemMessageAsync(roomId,
+                        $"{Context.User?.FindFirst(ClaimTypes.Name)?.Value!} has left");
+            if (sysMessage is not null)
+                await Clients.Group(roomId.ToString()).SendAsync("ReceiveMessage", sysMessage);
+        }
+
+        private int? GetUserId()
+        {
+            return int.TryParse(Context.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value, out int userId) ? userId : null;
         }
     }
 }
